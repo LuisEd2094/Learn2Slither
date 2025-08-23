a6a8376d710535a11db3f72b5c35c1d74439e1a4
Luis Soto <107001111+LuisEd2094@users.noreply.github.com>
2025-08-23 13:12:48 +0200

    feat: impl basic interactions. Solves #11 Solves #12 (#15)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from Python.play.play import play_game
+from Python.play.play import play_game_ai, play_game_user
 
 if __name__ == "__main__":
-    play_game(True)
+    play_game_user(True)
+    play_game_ai()
